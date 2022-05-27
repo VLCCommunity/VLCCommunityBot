770da50519d3d0af016aa798ee19d7968e3901e1
Yusuf R <notYusufR@gmail.com>
2022-05-27 12:10:33 +0000

    Set up MongoDB Atlas

--- index.js
+++ index.js
@@ -4,9 +4,21 @@
  *  The VLC name, logo, and all other branding are property of the Virtual Learning Center.
  *--------------------------------------------------------------------------------------------*/
 
-const { Client, Intents } = require("discord.js");
+// Imports
+
+const { Client, Intents } = require("discord.js"); // Discord
+const { MongoClient } = require("mongodb"); // Mongo
 
 const client = new Client({ intents: 98047 });
+const mongoDB = new MongoClient(process.env["MONGO_URI"], { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoDB.connect((err) => {
+  if (err) { console.log(`❌ Failed to connect to MongoDB.\n${err}`); }
+  console.log("✅ Connected to MongoDB.");
+});
+
+const botDB = mongoDB.db("BotDB");
+const guildsCollection = botDB.collection("Guilds");
 
 client.once("ready", async() => {
   console.log("✅ Connected to Discord.");
