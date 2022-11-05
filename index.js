47ed471f950bfe1397133b96bdd6ba1510b53328
Yusuf R <notYusufR@gmail.com>
2022-11-04 21:51:16 -0400

    Created basic files and failed to make bot run

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  *  The VLC name, logo, and all other branding are property of the Virtual Learning Center.
  *--------------------------------------------------------------------------------------------*/
 
-// Imports
+// Imports and Client Setup
 
 const { Client, Intents } = require("discord.js"); // Discord
 const { MongoClient } = require("mongodb"); // Mongo
@@ -12,13 +12,30 @@ const { MongoClient } = require("mongodb"); // Mongo
 const client = new Client({ intents: 98047 });
 const mongoDB = new MongoClient(process.env["MONGO_URI"], { useNewUrlParser: true, useUnifiedTopology: true });
 
+// MongoDB
+
 mongoDB.connect((err) => {
-  if (err) { console.log(`❌ Failed to connect to MongoDB.\n${err}`); }
-  console.log("✅ Connected to MongoDB.");
+  if (!err) {
+    console.log("✅ Connected to MongoDB.");
+  } else {
+    console.log(`❌ Failed to connect to MongoDB. ${err}`);
+  };
 });
 
 const botDB = mongoDB.db("BotDB");
-const guildsCollection = botDB.collection("Guilds");
+const guilds = botDB.collection("Guilds");
+
+client.login(process.env["TOKEN"]);
+
+module.exports = {
+  client,
+  guilds
+}
+
+const globals = require('./globals');
+module.exports.globals = globals;
+
+// Discord
 
 client.once("ready", async() => {
   console.log("✅ Connected to Discord.");
@@ -47,5 +64,3 @@ client.on("debug", (e) => {
     exec("kill 1");
   };
 });
-
-client.login(process.env["TOKEN"]);
